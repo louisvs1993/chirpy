236d5ddc14d6d521b3f9ece6cfafa673f3afa2f6
louisvs1993 <louisvansteelant@hotmail.com>
2026-07-02 15:24:56 +0200

    fileserver added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/louisvs1993/chirpy
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+	const filepathRoot = "."
+    mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	srv := &http.Server{
+        Addr:    ":" + port,
+        Handler: mux,
+    }
+
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
