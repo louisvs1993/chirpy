934a2df2f509a2d059e31b3234ce52b9821cf24d
louisvs1993 <louisvansteelant@hotmail.com>
2026-07-02 16:22:45 +0200

    Server readiness handler added

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	const port = "8080"
 	const filepathRoot = "."
     mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 
 	srv := &http.Server{
         Addr:    ":" + port,
